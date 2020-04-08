506dacf7fc1ff38ef98c05eb648c31599afa4630
mrlanu <mrlanu@gmail.com>
2020-04-07 22:29:08 -0500

    feat: reward the miners

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,6 @@
 # Initializing our (empty) blockchain list
+MINING_REWARDS = 10
+
 genesis_block = {
         'previous_hash': '',
         'index': 0,
@@ -59,6 +61,12 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
+    reward_transaction = {
+        'sender': 'MINING',
+        'recipient': owner,
+        'amount': MINING_REWARDS
+    }
+    open_transactions.append(reward_transaction)
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
